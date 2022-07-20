596d816f1b479e79a3318715d47e72744a1feaac
salmanpary <salmanpary@gmail.com>
2022-07-20 14:11:07 +0530

    lfadf

--- components/GetCritical/GetCritical.js
+++ components/GetCritical/GetCritical.js
@@ -7,7 +7,7 @@ const GetCritical = () => {
 
 
   
-   <div className="margin  bg-green w-[73.125rem] h-[23.375rem] rounded-2xl flex flex-col justify-center items-center text-center mt-[9rem] md:w-auto md:h-auto md:mx-4 md:p-5">
+   <div className="margin  bg-green w-[73.125rem] h-[23.375rem] rounded-2xl flex flex-col justify-center items-center text-center mt-[9rem] md:w-auto md:h-auto md:mx-4 md:p-5 md:hidden">
     <div className='relative'>
       <img src="/icons/smalldots.png" alt="" width={148} height={148} className="absolute top-[-2rem] left-[-7rem] md:hidden"/>
       <img src="/icons/smalldots.png" alt="" width={250} height={250} className="absolute bottom-[-9rem]  right-[-10rem] md:hidden"/>
