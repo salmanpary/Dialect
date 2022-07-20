6117d181e51ba7af3b30a3c0b2d8e60e76e8745a
salmanpary <salmanpary@gmail.com>
2022-07-20 14:24:47 +0530

    testing

--- components/CoursesOffered/TechStackCard.js
+++ components/CoursesOffered/TechStackCard.js
@@ -26,7 +26,7 @@ const TechStackCard = ({ icon, heading, content }) => {
     <div className="flex flex-col gap-4 border rounded-xl w-[349px] h-[290px] p-10 relative  ">
       {iconcomponent}
       {icon === "python" && (
-        <div className="absolute bg-green w-[349px] h-[11px] top-[-1px] left-[-1px] rounded-t-xl"></div>
+        <div className="absolute bg-green w-[349px] h-[11px] top-[-1px] left-[-1px] rounded-t-xl md:hidden"></div>
       )}
 
       <div className="text-2xl font-bold">{heading}</div>
