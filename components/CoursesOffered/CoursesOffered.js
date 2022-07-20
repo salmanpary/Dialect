5f24a62319a55914a0bb47c1fa547f33790dd63c
salmanpary <salmanpary@gmail.com>
2022-07-20 15:44:57 +0530

    new changes

--- components/CoursesOffered/CoursesOffered.js
+++ components/CoursesOffered/CoursesOffered.js
@@ -4,8 +4,18 @@ import LandingPageData from '../data/LandingPageData'
 import { useMediaQuery } from "usehooks-ts";
 const CoursesOffered = () => {
     const matches = useMediaQuery("(max-width: 576px)");
+    const [style,setstyle]=React.useState("margin")
+    React.useEffect(()=>{
+        if(!matches){
+            setstyle("margin")
+        }
+        else{
+            setstyle("")
+        }
+    }
+    ,[matches])
   return (
-    <div className={!matches?"margin":" "}>
+    <div className={style}>
         <h2 className='font-bold text-5xl flex justify-center items-center mt-[3.4375rem] md:text-center'>Courses Offered</h2>
         <div className="flex flex-wrap gap-[3.563rem] justify-center items-center mt-[5.125rem]">
             {
