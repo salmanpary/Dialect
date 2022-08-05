020cec6931b89a0df2c13eb570a84b73f0101bb9
salmanpary <salmanpary@gmail.com>
2022-08-05 13:53:39 +0530

    redirecting added

--- components/common/Buttons/EligiblityButton.js
+++ components/common/Buttons/EligiblityButton.js
@@ -1,15 +1,18 @@
-import React from 'react'
+import React from "react";
 
 const EligiblityButtton = () => {
   return (
-   <>
-   <a className='bg-green rounded-[80px] px-8 h-14 flex justify-center items-center shadow-2xl shadow-gray-500 md:w-[100%] cursor-pointer hover:bg-blue transition ease-in-out hover:-translate-y-1 hover:scale-110  duration-300'>
-  <span className='text-white font-extrabold text-sm'>
-  Check Eligiblity
-    </span> 
-   </a>
-   </>
-  )
-}
+    <>
+      <a
+        className="bg-green rounded-[80px] px-8 h-14 flex justify-center items-center shadow-2xl shadow-gray-500 md:w-[100%] cursor-pointer hover:bg-blue transition ease-in-out hover:-translate-y-1 hover:scale-110  duration-300"
+        href="https://o5ygjicxtso.typeform.com/to/fuA0j5LJ"
+      >
+        <span className="text-white font-extrabold text-sm">
+          Check Eligiblity
+        </span>
+      </a>
+    </>
+  );
+};
 
-export default EligiblityButtton
+export default EligiblityButtton;
--- components/common/Buttons/GetEarlyAccess.js
+++ components/common/Buttons/GetEarlyAccess.js
@@ -1,15 +1,16 @@
-import React from 'react'
+import React from "react";
 
 const EligiblityButtton = () => {
   return (
-   <>
-   <a className='text-green bg-white rounded-[80px] px-8 h-14 flex justify-center items-center shadow-2xl shadow-gray-500 md:w-[100%] cursor-pointer hover:bg-blue hover:text-white transition ease-in-out hover:-translate-y-1 hover:scale-110  duration-300'>
-  <span className=' font-extrabold text-sm'>
-Get Early Access
-    </span> 
-   </a>
-   </>
-  )
-}
+    <>
+      <a
+        className="text-green bg-white rounded-[80px] px-8 h-14 flex justify-center items-center shadow-2xl shadow-gray-500 md:w-[100%] cursor-pointer hover:bg-blue hover:text-white transition ease-in-out hover:-translate-y-1 hover:scale-110  duration-300"
+        href="https://o5ygjicxtso.typeform.com/to/fuA0j5LJ"
+      >
+        <span className=" font-extrabold text-sm">Get Early Access</span>
+      </a>
+    </>
+  );
+};
 
-export default EligiblityButtton
+export default EligiblityButtton;
