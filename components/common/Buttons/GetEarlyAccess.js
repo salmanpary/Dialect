bd5c5a32154e071254afdca558050a3c391b4ff9
salmanpary <salmanpary@gmail.com>
2022-08-16 14:11:14 +0530

    link changed

--- components/common/Buttons/GetEarlyAccess.js
+++ components/common/Buttons/GetEarlyAccess.js
@@ -5,7 +5,7 @@ const EligiblityButtton = () => {
     <>
       <a
         className="text-green bg-white rounded-[80px] px-8 h-14 flex justify-center items-center shadow-2xl shadow-gray-500 md:w-[100%] cursor-pointer hover:bg-blue hover:text-white transition ease-in-out hover:-translate-y-1 hover:scale-110  duration-300"
-        href="https://o5ygjicxtso.typeform.com/to/fuA0j5LJ"
+        href="https://gocollabo.typeform.com/to/foNXRbPs"
         target="blank"
       >
         <span className=" font-extrabold text-sm">Get Early Access</span>
