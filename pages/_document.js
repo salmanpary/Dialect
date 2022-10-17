c65ed2427c85b8bd1925a5cfb53d840316417db9
salmanpary <salmanpary@gmail.com>
2022-10-17 16:10:49 +0530

    tracking code

--- pages/_document.js
+++ pages/_document.js
@@ -63,6 +63,21 @@ coding courses in malayalam, flutter course in Malayalam, flutter online course 
             `,
             }}
           ></Script>
+          <Script
+            strategy="afterInteractive"
+            id="trackingcode2"
+            dangerouslySetInnerHTML={{
+              __html:`
+              window.smartlook||(function(d) {
+                var o=smartlook=function(){ o.api.push(arguments)},h=d.getElementsByTagName('head')[0];
+                var c=d.createElement('script');o.api=new Array();c.async=true;c.type='text/javascript';
+                c.charset='utf-8';c.src='https://web-sdk.smartlook.com/recorder.js';h.appendChild(c);
+                })(document);
+                smartlook('init', 'ffa8e84c0c407de203559a7a02dea78e6401ff4f', { region: 'eu' });
+              `,
+            }}
+          ></Script>
+
         </Head>
         <body>
           <Main />
